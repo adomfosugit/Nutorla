f029f4982380dc71d0bd57589dec15d242864dfd
adomfosugit <adomfosugit@gmail.com>
2023-05-12 08:04:22 +0000

    new designs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3674cce9667de05e"; }
+/******/ 		__webpack_require__.h = function() { return "c5ddab706ec0e568"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
